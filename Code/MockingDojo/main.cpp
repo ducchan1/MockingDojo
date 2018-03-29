ec9397a7c6ecf546ac06d0bf160628be02d94e19
Chantale Duchesne <chantale.duchesne@optelgroup.com>
2018-03-29 11:26:50 -0400

    added some data in main

--- Code/MockingDojo/main.cpp
+++ Code/MockingDojo/main.cpp
@@ -10,5 +10,15 @@ int main(int argc, char** argv)
     std::tr2::sys::path data_path("..\\MockingData");
     InternalDataUser data_user(data_path);
 
+    std::vector <std::string> loaded_data_names(data_user.getDataNames());
+    std::cout << "Found " << loaded_data_names.size() << " items" << std::endl;
+
+    for (const auto& name : loaded_data_names)
+    {
+        std::cout << "Item: " << name << std::endl;
+    }
+
+    system("pause");
+
     return 0;
 }
